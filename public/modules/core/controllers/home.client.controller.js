87edb61697cdd6232cf47a40b16e3666a735d618
Dalin Huang <hdltyty@163.com>
2015-02-12 03:09:19 -0500

    feb 12 update by DalinHuang

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -10,38 +10,38 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
             {  //1
                 icon: 'glyphicon-user',
                 colour:'btn-success',
-                total:'20,408',
+                total:'17,839',
                 description:'TOTAL CUSTOMERS'
             },
             {  //2
                 icon: 'glyphicon-calendar',
                 colour:'btn-primary',
-                total:'8,382',
+                total:'1,382',
                 description:'UPCOMING EVENTS'
             },
             {  //3
                 icon: 'glyphicon-edit',
                 colour:'btn-success',
-                total:'527',
+                total:'387',
                 description:'NEW CUSTOMERS IN 24H'
             },
             { //4
                 icon: 'glyphicon-record',
                 colour:'btn-info',
-                total:'85,000',
+                total:'78,300',
                 description:'EMAILS SENT'
             },
             { //5
                 icon: 'glyphicon-eye-open',
                 colour:'btn-warning',
-                total:'268',
-                description:'FOLLOW UPS REQUIRED'
+                total:'2,268',
+                description:'CURRENT CUSTOMERS ONLINE'
             },
             { //6
                 icon: 'glyphicon-flag',
                 colour:'btn-danger',
-                total:'348',
-                description:'REFERRALS TO MODERATE'
+                total:'1,548',
+                description:'LOCATION SHARED'
             }
         ];
 
